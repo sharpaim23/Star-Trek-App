dd9de4e4432f8e93c139618a506273c43fcf4af0
Aimee Sharp <sharpaim23@gmail.com>
2022-06-02 08:26:45 -0500

    First commit with main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+document.querySelector('button').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+  const alienName = document.querySelector('input').value
+  try{
+    const response = await fetch(`https://star-trek-api-sharp.herokuapp.com/api/${alienName}`)
+    const data = await response.json()
+    console.log(data);
+  }catch(error){
+    console.log(error);
+  }
+}
